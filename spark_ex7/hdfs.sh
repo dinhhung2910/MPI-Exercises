e005712678f85d048c6190bb498c1d27b43387a5
linhbm1511 <naegawolf09@gmail.com>
2020-05-08 21:38:44 +0700

    add: spark_ex7

---
+++ spark_ex7/hdfs.sh
@@ -0,0 +1,14 @@
+cd ~/Downloads/hadoop
+export PDSH_RCMD_TYPE=ssh
+bin/hdfs namenode -format
+sbin/start-dfs.sh
+sbin/start-yarn.sh
+bin/hdfs dfs -mkdir /wc/
+bin/hdfs dfs -mkdir /wc/input/
+bin/hdfs dfs -mkdir /kmer/
+bin/hdfs dfs -mkdir /kmer/input/
+bin/hdfs dfs -copyFromLocal ./hadoop/input1/bible+shakes.nopunc /wc/input 
+bin/hdfs dfs -copyFromLocal ../ecoli.fa /kmer/input
+# ~/Documents/TTPT/script.sh
+# bin/hdfs dfs -get /wc/input/wc_result output2
+# sort -r -k 2 ./output2/wc_result/part-00000 > ./output2/wc_result/sorted.txt && head ./output2/wc_result/sorted.txt
---
+++ spark_ex7/script.sh
@@ -0,0 +1,4 @@
+export SPARK_HOME=~/Documents/TTPT/spark-3.0.0-preview2-bin-hadoop2.7
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
+export PATH=$SPARK_HOME/python:$PATH
+$SPARK_HOME/bin/spark-submit ~/Documents/TTPT/test.py 
