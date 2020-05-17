fa3a988b8cff75d078ffb9fb141d0042e468cd7d
danhnd98 <danh.nd1998@gmail.com>
2020-05-17 20:32:21 +0700

    ex1

--- spark/ex1.py
+++ spark/ex1.py
@@ -8,10 +8,14 @@ if __name__ == "__main__":
     fileData = sc.textFile("./data/airports.dat")
     fileDataFilter = fileData.filter(lambda line: line.split(",")[3] == '"Papua New Guinea"')
     fileDataGroup = fileData.groupBy(lambda line: line.split(",")[3])
+    airpotsCount = fileData.map(lambda line: (line.split(",")[3], 1)).reduceByKey(lambda a,b:a +b)
+
     num = fileData.count()
     print("Number Airport " + str(num))
     numberInCountry = fileDataFilter.count()
     numberCountry = fileDataGroup.count()
+
+    airpotsCount.saveAsTextFile("./output/")
     print("Number Airport Papua New Guinea " + str(numberInCountry))
-    print("Number Airport Papua New Guinea " + str(numberCountry))
+    print("Number Country " + str(numberCountry))
     
--- spark_ex7/wc_localfile.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext
-import os
-sc = SparkContext("local", "count app")
-
-file = open(os.path.dirname(__file__)+"/bible+shakes.nopunc", "rt").read().replace("\n", " ").split(" ")
-words = sc.parallelize (file)
-
-counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile("hdfs://127.0.0.1:9000/wc/input/wc_result")
