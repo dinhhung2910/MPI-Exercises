bc90cc7bee31c7181aede09000b345766016b984
phamtrung <trungpq.sondra@gmail.com>
2020-05-17 22:42:35 +0700

    join

--- spark/ex1.py
+++ spark/ex1.py
@@ -1,3 +1,6 @@
+import findspark
+findspark.init()
+
 import sys
  
 from pyspark import SparkContext, SparkConf
@@ -18,4 +21,18 @@ if __name__ == "__main__":
     airpotsCount.saveAsTextFile("./output/")
     print("Number Airport Papua New Guinea " + str(numberInCountry))
     print("Number Country " + str(numberCountry))
+
+  
+    fileData = fileData.map(lambda line: line.split(",")).map(lambda x: (x[0], (x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13])))
+
+    routesRDD = sc.textFile("./data/routes.dat")
+    routesRDD = routesRDD.map(lambda line: line.split(",")).map(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5], x[6], x[7], x[8])))
+
+    joinRDD = fileData.join(routesRDD)
+
+    # flightGroup = routesRDD.groupBy(lambda line: line.split(",")[4])
+    # flightCount = routesRDD.map(lambda line: (line.split(",")[4], 1)).reduceByKey(lambda a,b:a +b)
+    # numberFlight = flightGroup.count()
+    # print("Number flying " + str(flyingCount))
+
     
