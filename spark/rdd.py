7e007eebe8def7f7978383b229dcfa87f4189285
phamtrung <trungpq.sondra@gmail.com>
2020-05-18 07:09:45 +0700

    final_spark

--- spark/rdd.py
+++ spark/rdd.py
@@ -1,6 +1,3 @@
-# import findspark
-# findspark.init()
-
 import sys
  
 from pyspark import SparkContext, SparkConf
@@ -11,14 +8,14 @@ if __name__ == "__main__":
     fileData = sc.textFile("./data/airports.dat")
     fileDataFilter = fileData.filter(lambda line: line.split(",")[3] == '"Papua New Guinea"')
     fileDataGroup = fileData.groupBy(lambda line: line.split(",")[3])
-    # airpotsCount = fileData.map(lambda line: (line.split(",")[3], 1)).reduceByKey(lambda a,b:a +b)
+    airpotsCount = fileData.map(lambda line: (line.split(",")[3], 1)).reduceByKey(lambda a,b:a +b)
 
     num = fileData.count()
     print("Number Airport " + str(num))
     numberInCountry = fileDataFilter.count()
     numberCountry = fileDataGroup.count()
 
-    # airpotsCount.saveAsTextFile("./output/")
+    airpotsCount.saveAsTextFile("./output/")
     print("Number Airport Papua New Guinea " + str(numberInCountry))
     print("Number Country " + str(numberCountry))
 
@@ -30,14 +27,9 @@ if __name__ == "__main__":
 
     joinRDD = fileData.join(routesRDD)
 
-    # flightGroup = routesRDD.groupBy(lambda line: line.split(",")[4])
-    # flightCount = routesFile.map(lambda line: (line.split(",")[4], 1)).reduceByKey(lambda a,b:a +b)
-    # flightCount.saveAsTextFile("./output/")
-    # numberFlight = flightCount.count()
-    # print("Number flying " + str(flightCount))
-
-    fightCount = joinRDD.map(lambda  x : (x[1][0][2], 1)).reduceByKey(lambda a,b:a +b)
+    fightCount = joinRDD.map(lambda  x : (x[1][0][2], 1)).reduceByKey(lambda a,b: a + b)
     fightCount.saveAsTextFile("./output1/")
+
     # fightCount = joinRDD.collect()
     # print(fightCount[0][1][0])
     
